079eff873419b2dcbc471108f3ac8a3e34caba7d
Gamel Ayodele <ayodelegamel@gmail.com>
2023-02-18 14:35:45 +0100

    latest commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -82,7 +82,7 @@ const getMe = asyncHandler(async (req, res) => {
 // Generate Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "60d",
+    expiresIn: "30d",
   });
 };
 
